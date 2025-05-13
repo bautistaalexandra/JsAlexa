d37d0f719400bce9b0f50a2ab18af878d2ae86fa
alexandra354-4 <bautistaalexandra354@gmail.com>
2025-05-13 11:51:51 -0500

    hola

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//comentario en JS 
+
+document.write("Yo soy un texto externo en JS");
+
+document.write("hr");
+
+document.write("<h2 style='color:red;'> Hola Alexa </h2>");
+
+var a,b,suma;
+a=10;
+b=50;
+suma=a+b;
+
+//window.alert("La suma de las dos variables es: "+suma); 
+
+document.write("<button onclick=\"window.alert('La suma de las dos variables es: " + suma + "');\">SUMA</button>");
